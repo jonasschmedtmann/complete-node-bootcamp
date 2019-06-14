f6252a67594071ca9a95d5da0ddf87d35aae61be
Jonas Schmedtmann <hello@jonas.io>
2019-06-14 16:52:42 +0100

    Fixed minor bug

--- 4-natours/after-section-14/controllers/authController.js
+++ 4-natours/after-section-14/controllers/authController.js
@@ -20,7 +20,7 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    secure: req.secure || req.headers('x-forwarded-proto') === 'https'
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
   });
 
   // Remove password from output
