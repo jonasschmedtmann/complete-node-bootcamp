a33ab75181b04ac976707ed944db7245e32cc952
weffelms <stacie.weffelmeyer@wwt.com>
2019-07-03 13:16:00 -0500

    routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const http = require('http')
+const url = require('url')
 
 ////////////////////////////////////////////
 //Files
@@ -32,7 +33,19 @@ const http = require('http')
 //Server
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server!')
+  const pathName = req.url
+  if(pathName === '/' || pathName === '/overview') {
+    res.end('This is the overview')
+  } else if (pathName === '/product') {
+    res.end('This is the product')
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    })
+    res.end('<h1>Page not found.</h1>')
+  }
+  // res.end('Hello from the server!')
 })
 
 server.listen(8000, '127.0.0.1', () => {
