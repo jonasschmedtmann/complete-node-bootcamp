a50478a69ebc3b208ec3d2651bca1773171827a5
weffelms <stacie.weffelmeyer@wwt.com>
2019-07-03 12:58:54 -0500

    creating a simple web server

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,8 @@
 const fs = require('fs')
+const http = require('http')
+
+////////////////////////////////////////////
+//Files
 
 //Blocking, synchronous way
 // const textInput = fs.readFileSync('./starter/txt/input.txt', 'utf-8')
@@ -10,16 +14,27 @@ const fs = require('fs')
 // console.log('File written')
 
 //non-blocking, asyncronous way
-fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('ERROR!')
-  fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2)
-    fs.readFile(`./starter/txt/append.txt`, 'utf-8', (err, data3) => {
-      console.log(data3)
-      fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('Your file has been written.👍')
-      })
-    })
-  })
+// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR!')
+//   fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2)
+//     fs.readFile(`./starter/txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3)
+//       fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('Your file has been written.👍')
+//       })
+//     })
+//   })
+// })
+// console.log('will read file!')
+
+////////////////////////////////////////////
+//Server
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server!')
 })
-console.log('will read file!')
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to Requests on port 8000')
+})
