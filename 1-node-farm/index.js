053fc5e867a787277fa0a0fe13f9453c764e0ca6
weffelms <stacie.weffelmeyer@wwt.com>
2019-07-31 13:44:35 -0500

    HTML Templating: filling the Templates

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -32,17 +32,42 @@ const url = require('url')
 
 ////////////////////////////////////////////
 //Server
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%FROM%}/g, product.from)
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+  output = output.replace(/{%ID%}/g, product.id)
+  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+  return output
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8')
+const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8')
+const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8')
+
 
 const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8')
+
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
   const pathName = req.url
   if(pathName === '/' || pathName === '/overview') {
-    res.end('This is the overview')
+    res.writeHead(200, {'Content-type': 'text/html'})
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+
+    res.end(output)
+    // res.end('This is the overview')
   } else if (pathName === '/product') {
     res.end('This is the product')
   } else if (pathName === '/api') {
+    res.writeHead(200, {'Content-type': 'application/json'})
     res.end(data)
     
   } else {
@@ -55,6 +80,6 @@ const server = http.createServer((req, res) => {
   // res.end('Hello from the server!')
 })
 
-server.listen(8000, '127.0.0.1', () => {
-  console.log('Listening to Requests on port 8000')
+server.listen(8003, '127.0.0.1', () => {
+  console.log('Listening to Requests on port 8003')
 })
