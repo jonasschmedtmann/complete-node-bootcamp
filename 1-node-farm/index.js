d49bbe9b85bd488f84e328f969e3596dfac95245
weffelms <stacie.weffelmeyer@wwt.com>
2019-07-31 15:56:04 -0500

    Using Modules 2: Our own Modules

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const http = require('http')
 const url = require('url')
-
+const replaceTemplate = require('./starter/modules/replaceTemplate')
 
 ////////////////////////////////////////////
 //Files
@@ -32,19 +32,6 @@ const url = require('url')
 
 ////////////////////////////////////////////
 //Server
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-  output = output.replace(/{%IMAGE%}/g, product.image)
-  output = output.replace(/{%PRICE%}/g, product.price)
-  output = output.replace(/{%FROM%}/g, product.from)
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-  output = output.replace(/{%QUANTITY%}/g, product.quantity)
-  output = output.replace(/{%DESCRIPTION%}/g, product.description)
-  output = output.replace(/{%ID%}/g, product.id)
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-  return output
-}
-
 const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`, 'utf-8')
 const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`, 'utf-8')
 const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/template-product.html`, 'utf-8')
