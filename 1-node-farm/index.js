cadb36468fa4dc3e0e79dfbbc5acb924c30a5eb7
weffelms <stacie.weffelmeyer@wwt.com>
2019-07-31 14:10:02 -0500

    Parsing variables from URLs

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -55,8 +55,9 @@ const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url
-  if(pathName === '/' || pathName === '/overview') {
+  const { query, pathname } = url.parse(req.url, true)
+  // const pathName = req.url
+  if(pathname === '/' || pathname === '/overview') {
     res.writeHead(200, {'Content-type': 'text/html'})
 
     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
@@ -64,9 +65,12 @@ const server = http.createServer((req, res) => {
 
     res.end(output)
     // res.end('This is the overview')
-  } else if (pathName === '/product') {
-    res.end('This is the product')
-  } else if (pathName === '/api') {
+  } else if (pathname === '/product') {
+    res.writeHead(200, {'Content-type': 'text/html'})
+    const product = dataObj[query.id]
+    const output = replaceTemplate(tempProduct, product)
+    res.end(output)
+  } else if (pathname === '/api') {
     res.writeHead(200, {'Content-type': 'application/json'})
     res.end(data)
     
