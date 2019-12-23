951dd50158f5e7950b51e1229db79635bf1de9d9
danju4rizzl <danjumashiwaju@gmail.com>
2019-12-23 13:06:14 +0200

    Changed  theme  🎨 + fied place holder issue

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -6,17 +6,17 @@ const url = require("url");
 /// SERVER
 
 const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/%{PRODUCTNAME}%/g, product);
-  output = output.replace(/%{IMAGE}%/g, product.image);
-  output = output.replace(/%{FROM}%/g, product.from);
-  output = output.replace(/%{NUTRIENTS}%/g, product.nutrients);
-  output = output.replace(/%{QUANTITY}%/g, product.quantity);
-  output = output.replace(/%{PRICE}%/g, product.price);
-  output = output.replace(/%{DESCRIPTION}%/g, product.description);
-  output = output.replace(/%{ID}%/, product.id);
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/, product.id);
 
   if (!product.organic)
-    output = output.replace(/%{NOT_ORGANIC}%/g, "not-organic");
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
   return output;
 };
 
@@ -39,11 +39,12 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  const pathName = req.url;
+  console.log(req.url);
 
+  const pathName = req.url;
   //  Overvew page
   if (pathName === "/" || pathName === "/overview") {
-    res.writeHead(200, { "content-type": "text/html" });
+    res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
       .map(el => replaceTemplate(tempCard, el))
@@ -56,7 +57,7 @@ const server = http.createServer((req, res) => {
     res.end("Product page is active");
   } else if (pathName === "/api") {
     //  API page
-    res.writeHead(200, { "content-type": "application/json" });
+    res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
   } else {
     // Error Page / Not found
