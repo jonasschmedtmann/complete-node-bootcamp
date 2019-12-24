abb6fcf9c0bac920d210986b1705952f20634273
danju4rizzl <danjumashiwaju@gmail.com>
2019-12-24 10:56:38 +0200

    included .prettierrc + .gitIgnore

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,47 +1,55 @@
-const fs = require("fs");
-const http = require("http");
-const url = require("url");
+// Core Modules
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
 
-// replaceTemplat is a custom module from the modules folder
-const replaceTemplate = require("./modules/replaceTemplate");
+// 3rd party Modules
+const slugify = require('slugify');
+
+// Developer Modules
+const replaceTemplate = require('./modules/replaceTemplate');
+
 ////////////////////////////////////////
 /// SERVER
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
+  'utf-8'
 );
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "utf-8"
+  'utf-8'
 );
 
 // This will get the content in json in the file or server
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 
 // This will trurn the string from the json server or file and turns it into JavaScript
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
   //  Overvew page
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, { "Content-type": "text/html" });
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
     const cardsHtml = dataObj
       .map(el => replaceTemplate(tempCard, el))
-      .join(" ");
-    const finalOutput = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+      .join(' ');
+    const finalOutput = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
     console.log(finalOutput);
     res.end(finalOutput);
     //  Product page
-  } else if (pathname === "/product") {
-    res.writeHead(200, { "Content-type": "text/html" });
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
     // the product will select the query id of each product page it is on using the [] nodation e.g Array[0] or Array[4]
     const product = dataObj[query.id];
@@ -49,19 +57,19 @@ const server = http.createServer((req, res) => {
     const output = replaceTemplate(tempProduct, product);
 
     res.end(output);
-  } else if (pathname === "/api") {
+  } else if (pathname === '/api') {
     //  API page
-    res.writeHead(200, { "Content-type": "application/json" });
+    res.writeHead(200, { 'Content-type': 'application/json' });
 
     res.end(data);
   } else {
     // Error Page / Not found
     res.writeHead(404, {
-      "Content-type": "text/html",
-      "Is-user-online": "fun"
+      'Content-type': 'text/html',
+      'Is-user-online': 'fun'
     });
 
-    res.end("<h1> Page not found </h1> ");
+    res.end('<h1> Page not found </h1> ');
   }
 });
 
@@ -69,7 +77,7 @@ const server = http.createServer((req, res) => {
 Use this setting to set the Server URL and PORT 📌
 */
 const PORT = 7646;
-const URL = "127.0.0.1";
+const URL = '127.0.0.1';
 /*
 Setting is above this line ☝️
 */
