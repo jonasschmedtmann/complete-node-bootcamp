accac5551f5d4db6e874cc3b3e24d79976fd53a2
danju4rizzl <danjumashiwaju@gmail.com>
2019-12-30 16:36:15 +0200

    Added funtions to API

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -4,19 +4,11 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'Hello from server-side', app: 'natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('you can post her to his Endpoint');
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -24,9 +16,31 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   });
-});
+};
+const getTour = (req, res) => {
+  console.log(req.params);
+  // Using * 1 ie req.params.id * 1 is a type converstion
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
 
-app.post('/api/v1/tours', (req, res) => {
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+
+    data: {
+      tour
+    }
+  });
+};
+
+const createTour = (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -44,7 +58,48 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
+const updateTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID'
+    });
+  }
+
+  res.status(200).json({
+    status: 'succsess',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  });
+};
+
+const deleteTour = (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid ID'
+    });
+  }
+
+  res.status(204).json({
+    status: 'succsess',
+    data: {
+      tour: null
+    }
+  });
+};
+
+app.get('/api/v1/tours', getAllTours);
+// Use : sign to create a variable param
+app.get('/api/v1/tours/:id', getTour);
+
+app.post('/api/v1/tours', createTour);
+
+app.patch('/api/v1/tours/:id', updateTour);
+
+app.delete('/api/v1/tours/:id', deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
