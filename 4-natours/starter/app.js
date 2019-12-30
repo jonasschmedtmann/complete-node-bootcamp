f3d748e1ee1749e9703de7f4d3f09bd64511637d
danju4rizzl <danjumashiwaju@gmail.com>
2019-12-30 16:45:31 +0200

    Refactored the API

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -91,15 +91,23 @@ const deleteTour = (req, res) => {
   });
 };
 
-app.get('/api/v1/tours', getAllTours);
-// Use : sign to create a variable param
-app.get('/api/v1/tours/:id', getTour);
+// *NOTE* : sign to create a variable param ie (:id || :anyName || :x)
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
 
-app.post('/api/v1/tours', createTour);
+app
+  .route('/api/v1/tours')
+  .get(getAllTours)
+  .post(createTour);
 
-app.patch('/api/v1/tours/:id', updateTour);
-
-app.delete('/api/v1/tours/:id', deleteTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => {
