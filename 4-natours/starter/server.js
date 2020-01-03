80f6fd08fff51705e0be3c271a77d0bce8f4fd37
danju4rizzl <danjumashiwaju@gmail.com>
2020-01-03 16:00:13 +0200

    Added dotenv dependency

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,12 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: 'config.env' });
 const app = require('./app');
 
-const port = 3000;
+// console.log(app.get('env'));
+// console.log(process.env);
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on http://127.0.0.1:${port}...`);
 });
