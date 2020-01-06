7d45668c729fe80bc33b9e6cdb50e10da1fb692f
danju4rizzl <danjumashiwaju@gmail.com>
2020-01-06 15:11:16 +0200

    Added pagination to API

--- 4-natours/starter/contollers/tourController.js
+++ 4-natours/starter/contollers/tourController.js
@@ -30,12 +30,23 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
-    // 3) fields Limiting
+    // 3) Fields Limiting
     if (req.query.fields) {
       const fields = req.query.fields.split(',').join(' ');
       query = query.select(fields);
     } else {
       query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     //Execute Query
