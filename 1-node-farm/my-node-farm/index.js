f5fe9d5372b786852912c4ffab532138f9c18a67
KevinRobertAndrews <49161923+Kapppow@users.noreply.github.com>
2020-04-21 17:18:51 -0400

    Write a simple Node.js server

--- 1-node-farm/my-node-farm/index.js
+++ 1-node-farm/my-node-farm/index.js
@@ -1,23 +1,16 @@
-const fs = require("fs");
+const fs = require("fs"); // File System
+const http = require("http"); // HTTP Handling
 
-// Blocking, synchronous way
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// const textOut = `Hello, let me tell you all about the avocado! ${textIn}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("File was written!");
+///////////////////////////////
+// FILES
 
-// Non-blocking, asynchronous way with callbacks
-// aka CALLBACK HELL!!!
-fs.readFile("./txt/starttttt.txt", "utf-8", (err, data1) => {
-  if (err) return console.log(err);
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      console.log(data3);
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("File has been written!");
-      });
-    });
-  });
+////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+  console.log(req);
+  res.end("Hello from the server!");
 });
-console.log("Will read file!");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening on PORT 8000!");
+});
