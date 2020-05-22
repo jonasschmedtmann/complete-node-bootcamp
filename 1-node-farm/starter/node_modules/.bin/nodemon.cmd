cc6758bbd65ae710ed6637e436cd06b9e6ac9969
vasudevapitta <tizzicboy@gmail.com>
2020-05-21 22:29:22 -0500

    remove node_modules folder

--- 1-node-farm/starter/node_modules/.bin/nodemon.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\nodemon\bin\nodemon.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\nodemon\bin\nodemon.js" %*
-)
