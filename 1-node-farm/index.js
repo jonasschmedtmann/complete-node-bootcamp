d2f851246999ca7006797ea4cb1eb86c22479f3e
matthewmjm <matthewmjm@yahoo.com>
2020-07-03 08:55:45 -0600

    create a module for NodeFarm project

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,24 +1,12 @@
 const fs = require('fs');   //requires the file_system module
 const http = require('http') // gives us networking capability 
 const url = require('url'); // gives us routing capability 
+const replaceTemplate = require('./modules/replaceTemplate');
 const { runInNewContext } = require('vm');
 const { resourceUsage } = require('process');
 
 
 // SERVER
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-        output = output.replace(/{%IMAGE%}/g, product.image);
-        output = output.replace(/{%PRICE%}/g, product.price);
-        output = output.replace(/{%FROM%}/g, product.from);
-        output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-        output = output.replace(/{%QUANTITY%}/g, product.quantity);
-        output = output.replace(/{%DESCRIPTION%}/g, product.description);
-        output = output.replace(/{%ID%}/g, product.id);
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
-
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
