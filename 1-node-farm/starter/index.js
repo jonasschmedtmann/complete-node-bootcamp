df11ab9469771a6eee11c0acda78cb60b9a2967b
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-10-17 22:09:12 +0100

    routing server

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 //Blocking, synchronous code execution
 // const textIN = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -31,9 +32,21 @@ const http = require('http');
 
 ////////////////////////////////
 //SERVER
-const server = http.createServer(
-    (req, res)=>{
-        res.end('Hello from the server!');
+const server = http.createServer((req, res)=>{
+       const pathName = req.url;
+
+       if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the OVERVIEW');
+       } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+       } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found</h1>');
+       }
+        // res.end('Hello from the server!');
     });
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening on requests on port 8000');
