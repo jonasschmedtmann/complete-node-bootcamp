c668c134bc50a31c3a9b4882600eb439230e98a7
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-10-18 15:33:15 +0100

    HTML Templates filling

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -33,6 +33,24 @@ const url = require('url');
 ////////////////////////////////
 //SERVER
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic)  output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-8');
+
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);  
@@ -42,15 +60,27 @@ const dataObj = JSON.parse(data);
 const server = http.createServer((req, res)=>{
        const pathName = req.url;
 
+//Overview Page
        if(pathName === '/' || pathName === '/overview'){
-        res.end('This is the OVERVIEW');
+        res.writeHead(200, {'Content-type': 'text/html'})  //200 == okay, object for JSON
+       
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('(%PRODUCT_CARDS%)', cardsHtml);
+       
+        res.end(output);
+
+// Product page
        } else if (pathName === '/product') {
         res.end('This is the PRODUCT');
+
+//API
        } else if (pathName === '/api'){
         //res.end('API') new route
         res.writeHead(200, {'Content-type': 'application/json'})  //200 == okay, object for JSON
         res.end(data);
        }
+
+//Not found
        else {
         res.writeHead(404, {
             'Content-type': 'text/html',
