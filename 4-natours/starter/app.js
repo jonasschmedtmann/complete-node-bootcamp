d9cda30143d6919343a1659a5246ec63f1659d68
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-10-23 22:58:36 +0100

    express basic routing

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,20 @@
+const express = require ("express");
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res
+    .status(200)
+    .json({message:'Hello from the server side!👋 📻', app: 'Natours'})
+});
+
+app.post('/', (req, res) =>{
+    res
+    .send('You can post to this endpoint... 📑')
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
+
