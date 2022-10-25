c3dfcd1897676cf56b3e6f941b63182128f5a004
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-10-25 23:02:42 +0100

    refactor controller object get

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,31 +1,52 @@
 const Tour = require('../models/tourModel');
 
-
-
 //ROUTE HANDLERS
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    // rquestedAt: req.requestTime,
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      rquestedAt: req.requestTime,
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
-exports.getTour = (req, res) => {
-  console.log(req.params);
+
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 
   const id = req.params.id * 1;
-//   const tour = tours.find((el) => el.id === id);
+  //   const tour = tours.find((el) => el.id === id);
 
-//   res.status(200).json({
-//     status: 'success',
-//     data: {
-//       tour,
-//     },
-//   });
+  //   res.status(200).json({
+  //     status: 'success',
+  //     data: {
+  //       tour,
+  //     },
+  //   });
 };
 
 exports.createTour = async (req, res) => {
@@ -41,7 +62,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!'
+      message: 'Invalid data sent!',
     });
   }
 };
