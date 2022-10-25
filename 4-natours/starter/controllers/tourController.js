f1ea7b79a98aa1db590f28e6d104c7dcf096bf38
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-10-25 23:17:32 +0100

    refactor controller object delete

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -77,9 +77,17 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: 'null',
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+     await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: 'null',
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
