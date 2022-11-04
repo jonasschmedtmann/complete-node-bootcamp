b0aa086fcce63c11094ce3a0a1e2a0da55086b23
marcoanicosta <marcolinonafafe@hotmail.co.uk>
2022-11-04 23:49:45 +0000

    aggregation middleware

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -143,51 +143,49 @@ exports.getTourStats = async (req, res) => {
   }
 };
 
-
 exports.getMonthlyPlan = async (req, res) => {
-try {
-  const year = req.params.year *1;
+  try {
+    const year = req.params.year * 1;
 
-  const plan = await Tour.aggregate ([
-    {
-      $unwind: '$startDates'
-    },
-    {
-      $match: {
-        startDates: { 
-          $gte: new Date(`${year}-01-01`),
-          $lte: new Date(`${year}-12-31`)
-        }
-      }
-    },
-    {
-      $group: {
-        _id: { $month: '$startDates' },
-        numToursStarts: { $sum: 1 },
-        tours: { $push: '$name' },
-      }
-    },
-    {
-      $addFields:{ month: '$_id' },
-    },
-    {
-      $project: {
-        _id:0
-      }
-    },
-    {
-        $sort: { numTourStarts: -1}  
-    },
-  ]);
- 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      plan,
-    },
-  });
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numToursStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+    ]);
 
-} catch (err) {
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
     res.status(404).json({
       status: 'fail',
       message: err,
--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -91,6 +91,13 @@ tourSchema.post(/^find/, function (doc, next) {
   console.log(docs);
   next();
 });
+
+//AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
