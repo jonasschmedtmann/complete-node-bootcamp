ae41919f5739a4ac28e60dc5f7ed449f677e59f5
khyndra <arhs@ua.pt>
2023-03-21 19:36:42 +0000

    Parsing Values from URL

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -38,7 +38,7 @@ const replaceTemplate = (temp, product) => {
   output = output.replace(/{%IMAGE%}/g, product.image);
   output = output.replace(/{%PRICE%}/g, product.price);
   output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%NUTRIENTSNAME%}/g, product.nutrients);
   output = output.replace(/{%QUANTITY%}/g, product.quantity);
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
   output = output.replace(/{%ID%}/g, product.id);
@@ -64,12 +64,11 @@ const data = fs.readFileSync(__dirname + "/dev-data/data.json", "utf-8");
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-  console.log(req.url); // http://127.0.0.1:8000/overview?id=12&abc=567 req.url=/overview?id=12&abc=567
-
-  const pathName = req.url;
+  //console.log(req.url); // http://127.0.0.1:8000/overview?id=12&abc=567 req.url=/overview?id=12&abc=567
+  const { query, pathname } = url.parse(req.url, true);
 
   //Overview Page
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
     const cardsHtml = dataObj
@@ -80,11 +79,14 @@ const server = http.createServer((req, res) => {
     res.end(output);
 
     //Product Page
-  } else if (pathName === "/product") {
-    res.end("This is the Product");
+  } else if (pathname === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     //API
-  } else if (pathName === "/api") {
+  } else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
 
