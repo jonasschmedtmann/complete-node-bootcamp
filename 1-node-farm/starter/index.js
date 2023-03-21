2f4cbb5537739fa382ca351e04dccee31d062a56
khyndra <arhs@ua.pt>
2023-03-21 15:48:36 +0000

    HTML templating
    Building the templates and filling the templates

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -32,6 +32,34 @@ console.log("Will read file!");
 
 //////////////////////////SERVER///////////////////////
 //Top-level code (outside the callback functions) - Called only once when the code starts
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+const tempOverview = fs.readFileSync(
+  __dirname + "/templates/template-overview.html",
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  __dirname + "/templates/template-card.html",
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  __dirname + "/templates/template-product.html",
+  "utf-8"
+);
+
 const data = fs.readFileSync(__dirname + "/dev-data/data.json", "utf-8");
 const dataObj = JSON.parse(data);
 
@@ -40,16 +68,30 @@ const server = http.createServer((req, res) => {
 
   const pathName = req.url;
 
+  //Overview Page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the Overview");
+    res.writeHead(200, { "Content-type": "text/html" });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    res.end(output);
+
+    //Product Page
   } else if (pathName === "/product") {
     res.end("This is the Product");
+
+    //API
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
+
+    //Not Found
   } else {
     res.writeHead(404, {
-      "Content-type": "text.html",
+      "Content-type": "text/html",
       "my-own-header": "hello-world",
     }); //appears in the network tab
     //Response headers must be always set up before sending the response
