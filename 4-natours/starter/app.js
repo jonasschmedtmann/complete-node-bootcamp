edbba6e774f19f6978b907d2bf7465ae49f3e5c0
lchmielewskistuart <l.chmielewski@stuart.com>
2023-07-26 08:50:26 +0200

    after section 4

---
+++ 4-natours/starter/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App runing on port ${port}...`);
+});
