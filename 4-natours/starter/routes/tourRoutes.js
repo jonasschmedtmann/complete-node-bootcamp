08127110394f89db1d5c6c54c88dd1353be38854
lchmielewskistuart <l.chmielewski@stuart.com>
2023-08-09 11:52:54 +0200

    after section 6
    after section 6

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const tourController = require('./../controllers/tourController');
+const tourController = require('../controllers/tourController');
+
 const router = express.Router();
 
 router.param('id', tourController.checkID);
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
