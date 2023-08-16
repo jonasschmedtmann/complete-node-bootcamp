f99b773b480fafbdb4d478829606a5780f7ea393
lchmielewskistuart <l.chmielewski@stuart.com>
2023-08-16 11:30:02 +0200

    after section 93

--- 4-natours/starter/modules/tourModel.js
+++ 4-natours/starter/modules/tourModel.js
@@ -5,16 +5,53 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: [true, 'You cannot have to tours with the same name'],
+    unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Tour must have a diffuculty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: { type: Number },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour must have description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Tour must have cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
