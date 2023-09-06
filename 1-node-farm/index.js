f1551e84923f9adf44d2735a1f2fd6a72269952a
rhysplloyd <79843065+rhysplloyd@users.noreply.github.com>
2023-09-06 16:04:42 +0100

    Simple API

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -34,6 +34,12 @@ const http = require("http");
 //////////////////////////////////////////////////////////////////
 // SERVER
 
+const data = fs.readFileSync(
+  `${__dirname}/starter/dev-data/data.json`,
+  "utf-8"
+);
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -41,6 +47,9 @@ const server = http.createServer((req, res) => {
     res.end("This is the overview.");
   } else if (pathName === "/product") {
     res.end("This is the product.");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
