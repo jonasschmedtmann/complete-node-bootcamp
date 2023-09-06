6c7c6e6fb23807078a80804a1ce9beb377e4e87f
rhysplloyd <79843065+rhysplloyd@users.noreply.github.com>
2023-09-06 14:52:24 +0100

    Routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -35,6 +35,20 @@ const http = require("http");
 // SERVER
 
 const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the overview.");
+  } else if (pathName === "/product") {
+    res.end("This is the product.");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found.</h1>");
+  }
+
   console.log(req);
   res.end("Hello from the server!");
 });
