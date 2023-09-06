22698c401e192148c9463633575a7b59b6f0954a
lchmielewskistuart <l.chmielewski@stuart.com>
2023-09-06 11:14:13 +0200

    completed mongoose

--- 4-natours/starter/modules/tourModel.js
+++ 4-natours/starter/modules/tourModel.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -9,6 +10,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'Tour must have a duration'],
@@ -54,6 +56,10 @@ const tourSchema = new mongoose.Schema(
       select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -64,6 +70,34 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+// Query MIDDLEWARE
+tourSchema.pre('/^find/', function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post('/^find/', function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} `);
+  console.log(docs);
+  next();
+});
+
+// DOCUMENT MIDDLEWARE: before .save() and .create()
+// tourSchema.pre('save', function (next) {
+//   this.slug = slugify(this.name, { lower: true });
+//   next();
+// });
+
+// tourSchema.pre('save', function (next) {
+//   console.log(`Will save new tour: ${this.name}`);
+//   next();
+// });
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
