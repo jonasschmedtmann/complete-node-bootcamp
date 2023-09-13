cb57001b227a902c0f52605fcc3edaec521e029f
lchmielewskistuart <l.chmielewski@stuart.com>
2023-09-13 10:02:15 +0200

    after section 8

--- 4-natours/starter/modules/tourModel.js
+++ 4-natours/starter/modules/tourModel.js
@@ -1,6 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -9,6 +11,9 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal to 40 char'],
+      minlength: [10, 'A tour name must have at least 10 char'],
+      // validate: [validator.isAlpha, 'Tour name must only contain char'],
     },
     slug: String,
     duration: {
@@ -22,10 +27,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'Tour must have a diffuculty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is easy, medium or difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Ratings start at 1'],
+      max: [5, 'Max Rating is 5'],
     },
     ratingsQuantity: {
       type: Number,
@@ -35,7 +46,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price'],
     },
-    priceDiscount: { type: Number },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        // this only has access to NEW document
+        validator: function (val) {
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) must be below reg price',
+      },
+    },
     summary: {
       type: String,
       trim: true,
@@ -71,24 +91,31 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
-// Query MIDDLEWARE
-// tourSchema.pre('/^find/', function (next) {
-//   this.find({ secretTour: { $ne: true } });
-//   this.start = Date.now();
-//   next();
-// });
-
-// tourSchema.post('/^find/', function (docs, next) {
-//   console.log(`Query took ${Date.now() - this.start} `);
-//   console.log(docs);
-//   next();
-// });
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
 
 // DOCUMENT MIDDLEWARE: before .save() and .create()
-// tourSchema.pre('save', function (next) {
-//   this.slug = slugify(this.name, { lower: true });
-//   next();
-// });
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Query MIDDLEWARE
+tourSchema.pre('/^find/', function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post('/^find/', function (docs, next) {
+  console.log(`Query took ${Date.now() - this.start} `);
+  console.log(docs);
+  next();
+});
 
 // tourSchema.pre('save', function (next) {
 //   console.log(`Will save new tour: ${this.name}`);
