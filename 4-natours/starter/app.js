ce9f66c33945eddbf42b8a61ed0e1baff804dc7d
lchmielewskistuart <l.chmielewski@stuart.com>
2023-09-13 10:47:17 +0200

    after lesson 112

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -24,4 +24,11 @@ app.use((req, res, next) => {
 app.use(`/api/${version}/users`, userRouter);
 app.use(`/api/${version}/tours`, tourRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;
