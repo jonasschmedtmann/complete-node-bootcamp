6e069f9ca5b02fb43b3b9fb07739f8e2ef0ac22e
haliluysal <haliluysal@gmail.com>
2023-12-16 08:21:35 -0500

    write file functionality

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,7 @@
 const fs = require("fs")
 
-const textIn = fs.readFileSync(".//txt//input.txt", "utf-8")
+const textIn = fs.readFileSync(".//txt//output.txt", "utf-8")
 console.log(textIn)
+
+const textOut = `nice feature for es6 like python ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync(".//txt//output.txt", textOut)
