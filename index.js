dc451a5102a95f29d944085dc8ba5017fdda5c35
Catherine Wheeler-Mushlin <107967088+CatW-M@users.noreply.github.com>
2023-12-29 11:11:50 -0700

    html templating

--- index.js
+++ index.js
@@ -5,16 +5,13 @@ const url = require('url');
 ////////FILES
 //Blocking Syncronous Way
 // const textIn = fs.readFileSync('./1-node-farm/starter/txt/input.txt', 'utf-8');
-
 // console.log(textIn);
-
 // const textOut = `This is what we know about the avocado ${textIn}.\nCreated on ${Date.now()}`;
 
 // fs.writeFileSync('./1-node-farm/starter/txt/output.txt', textOut);
 // console.log('File written!');
 
 //Non-blocking asynchronous way
-
 // fs.readFile('./1-node-farm/starter/txt/start.txt', 'utf-8', (err, data1) => {
 //   if (err) return console.log('ERROR!🥵')
 //   fs.readFile(`./1-node-farm/starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
@@ -30,7 +27,26 @@ const url = require('url');
 // });
 // console.log("Will read file!")
 
-/////////SERVER
+/////////////////////BREAK///////////////////////////
+
+
+//SERVER
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+ 
+  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+}
+const tempOverview = fs.readFileSync('./1-node-farm/starter/templates/template-overview.html', 'utf-8');
+const tempCard = fs.readFileSync('./1-node-farm/starter/templates/template-card.html', 'utf-8');
+const tempProduct = fs.readFileSync('./1-node-farm/starter/templates/template-product.html', 'utf-8');
 
 const data = fs.readFileSync('./1-node-farm/starter/dev-data/data.json', 'utf-8');
 const dataObj = JSON.parse(data);
@@ -38,19 +54,29 @@ const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+//Overview page
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the OVERVIEW!!');
+    res.writeHead(200, { 'Content-type': 'text/html'});
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+
+//Product page
   } else if (pathName === '/product') {
     res.end('This is the PRODUCT!')
+
+//API
   } else if (pathName === '/api') {
-  
       res.writeHead(200, { 'Content-type': 'application/json'});
       res.end(data);
 
+//Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
-      'my-own-header': 'Hello World'
+      'my-own-header': 'hello-world'
     });
     res.end('<h1>PAGE NOT FOUND!</h1>');
   }
