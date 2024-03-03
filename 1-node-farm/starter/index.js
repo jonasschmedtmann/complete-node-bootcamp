1346d2bdb95dbe0950fb639ae44f6c5eb87e48f4
mryunt02 <mrthresh58@gmail.com>
2024-03-03 15:31:14 +0300

    node lesson2-writeFileSync

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,3 +1,8 @@
 const fs = require("fs");
 const hello = "hello world";
-console.log(fs.readFileSync("final"));
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8"); // readFileSync("./path","") takes two arguments, first one is reading file we want,second is character encoding
+console.log(textIn);
+const textOut = `About avocado ${textIn}.\n Created on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut); // writeFileSync("./path","text") takes two arguments, first one  is the path to the file where the data will be written, second is the data that will be written to the file. if first data doesn't exist writeFileSync create a new file.
+const readTextOut = fs.readFileSync("./txt/output.txt", "utf-8");
+console.log(readTextOut);
