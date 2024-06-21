f33bb23a37d9fb5027743776188b87b8b86cae31
pawelek <k.pawelek@vp.pl>
2024-06-21 13:17:14 +0200

    add async

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,10 +1,26 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// sync
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
-console.log(textIn);
+// console.log(textIn);
 
-const textOut = `cos tam avodaco ${textIn}.\nCreated on ${Date.now()}`;
+// const textOut = `cos tam avodaco ${textIn}.\nCreated on ${Date.now()}`;
 
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("zrobione");
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("zrobione");
+
+//async
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("your file has been written.");
+      });
+    });
+  });
+});
